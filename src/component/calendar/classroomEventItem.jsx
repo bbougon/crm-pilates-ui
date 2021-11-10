f027ea03cb5c3011aceb666267a466a42c963601
Bertrand Bougon <bertrand.bougon@gmail.com>
2021-11-10 14:53:11 +0100

    refacto extract session details

--- src/component/calendar/classroomEventItem.jsx
+++ src/component/calendar/classroomEventItem.jsx
@@ -1,23 +1,11 @@
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {Grid} from "@material-ui/core";
-import {
-    Box,
-    Card,
-    CardContent,
-    CardHeader,
-    Chip,
-    ClickAwayListener,
-    Fade,
-    Popper,
-    Switch,
-    Typography
-} from "@mui/material";
+import {Box, Card, ClickAwayListener, Fade, Popper, Typography} from "@mui/material";
 import {blue} from "@mui/material/colors";
 import {format} from "date-fns";
 import * as React from "react";
 import {useState} from "react";
-import {sessionCheckin} from "../../features/sessionsSlice";
-import {useDispatch} from "react-redux";
+import {SessionDetails} from "../classroom/sessionDetails";
 
 const theme = createTheme({
     typography: {
@@ -49,69 +37,6 @@ const theme = createTheme({
     },
 });
 
-const SessionAttendee = ({attendee, session}) => {
-
-    const [attendeeLabelStatus] = useState(attendee.attendance === "REGISTERED" ? 'R' : 'C')
-    const [attendeeLabelColor] = useState(attendee.attendance === "REGISTERED" ? 'primary' : 'success')
-
-    const dispatch = useDispatch();
-
-    const  onSessionCheckin = async (e) => {
-        if(e.target.checked) {
-            const checkin = Object.assign({}, session, attendee)
-            await dispatch(sessionCheckin(checkin))
-        }
-    }
-
-    return (
-        <Grid container direction="row">
-            <Grid item xs={8} md={8}>
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'flex-start'
-                }}>
-                    <Typography variant="body1">
-                        {attendee.firstname.concat(" ").concat(attendee.lastname)}
-                    </Typography>
-                </Box>
-            </Grid>
-            <Grid item xs={4} md={4}>
-                <Grid container direction="row">
-                    <Grid item xs={6} md={6} sx={{
-                        display: 'flex',
-                        justifyContent: 'flex-end'
-                    }}>
-                        <ThemeProvider theme={theme}>
-                            <Switch size="small" defaultChecked={attendee.attendance === "CHECKED_IN"}
-                                    color={attendeeLabelColor} onChange={onSessionCheckin}/>
-                        </ThemeProvider>
-                    </Grid>
-                    <Grid item xs={6} md={6} sx={{
-                        display: 'flex',
-                        justifyContent: 'flex-end'
-                    }}>
-                        <ThemeProvider theme={theme}>
-                            <Chip size="small" label={attendeeLabelStatus} color={attendeeLabelColor}/>
-                        </ThemeProvider>
-                    </Grid>
-
-                </Grid>
-            </Grid>
-        </Grid>
-    )
-}
-
-
-const SessionAttendees = ({session}) => {
-
-    const content = session.attendees.map((attendee) => (<SessionAttendee key={attendee.id} attendee={attendee} session={session} />))
-    return (
-        <Grid container>
-            {content}
-        </Grid>
-    )
-}
-
 
 export const ClassroomEventItem = ({classroom: session}) => {
 
@@ -131,20 +56,15 @@ export const ClassroomEventItem = ({classroom: session}) => {
         <Grid container>
             <Popper open={open} anchorEl={anchorEl} placement="right-start" transition>
                 {({TransitionProps}) => {
-                    let subheader = format(session.schedule.start, "yyyy-MM-dd H:mm").concat(" / ").concat(format(session.schedule.stop, "yyyy-MM-dd H:mm"));
                     return (
                         <ThemeProvider theme={theme}>
                             <Fade {...TransitionProps} timeout={350}>
                                 <Card sx={{minWidth: 450, maxWidth: 600, display: 'flex'}}>
                                     <ClickAwayListener onClickAway={closeSessionDisplay}
-                                                       disableReactTree={true} >
+                                                       disableReactTree={true}>
+
                                         <Box sx={{display: 'flex', flexDirection: 'column'}}>
-                                            <CardHeader title={session.name}
-                                                        subheader={subheader}
-                                                        component="div"/>
-                                            <CardContent>
-                                                <SessionAttendees session={session} />
-                                            </CardContent>
+                                            <SessionDetails session={session}/>
                                         </Box>
                                     </ClickAwayListener>
                                 </Card>
